1ad0d5822575e5e8475acc58d202ad0139244612
Masahiro Sakai <masahiro.sakai@gmail.com>
2024-11-10 23:21:31 +0900

    remove unnecessary imports of ChatOpenAI

--- experts/programming_expert.py
+++ experts/programming_expert.py
@@ -1,7 +1,6 @@
 from experts.base_expert import BaseExpert
 
 from langchain import PromptTemplate, OpenAI, LLMChain
-from langchain.chat_models import ChatOpenAI
 
 
 class ProgrammingExpert(BaseExpert):
